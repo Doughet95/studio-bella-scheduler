a00363bcad9775fd4397b0e6f81853867dd06970
Doughet95 <douglas.smart20@gmail.com>
2026-07-24 12:16:39 -0300

    fix: add missing ThemeProvider import in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Playfair_Display } from 'next/font/google'
 import './globals.css'
 import { Providers } from '@/components/providers'
 import { Toaster } from '@/components/ui/toaster'
+import { ThemeProvider } from '@/components/theme-provider'
 
 const inter = Inter({
   subsets: ['latin'],
